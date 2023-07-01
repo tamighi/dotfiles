7649812069a752c439ca8119625258b7ccd66300
thomas <tamighi@hotmail.com>
2023-07-01 13:33:35 +0200

    some fixes

--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 ### install npm for Mason
 sudo apt install curl
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
-source ${HOME}/.bashrc
+source ${HOME}/.nvm/nvm.sh
 nvm install node

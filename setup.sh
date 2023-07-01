9c058aea0354e72c0535063a07207ff72f15056a
thomas <tamighi@hotmail.com>
2023-07-01 15:28:19 +0200

    fix(nvim): Silent ts autotags errors

--- setup.sh
+++ setup.sh
@@ -11,7 +11,8 @@ ln -sn ${HOME}/dotfiles/nvim ${HOME}/.config/nvim
 ### setup packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+echo "nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'"
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' > /dev/null 2>&1
 ### install npm for Mason
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
 source ${HOME}/.nvm/nvm.sh

cbd5d672a8b96c586870745d721f98a6640db455
thomas <tamighi@hotmail.com>
2023-07-01 12:30:45 +0200

    fix setup

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 ## installation
 sudo snap install nvim --classic
 ## symbolic link
-ln -s ${HOME}/dotfiles/nvim ${HOME}/.config/nvim
+ln -sn ${HOME}/dotfiles/nvim ${HOME}/.config/nvim
 ## Configure Packer
 ### install npm for Mason
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.3/install.sh | bash
-source ~/.bashrc
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.3/install.sh
+bash
+source ${HOME}/.bashrc
 nvm install node
 ### setup packer
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'

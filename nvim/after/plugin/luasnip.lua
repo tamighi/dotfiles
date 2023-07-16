585d0fafe4cde2a80fe72844637de53b83d370ff
thomas <tamighi@hotmail.com>
2023-07-16 08:35:08 +0200

    Dont import js to ts anymore

--- nvim/after/plugin/luasnip.lua
+++ nvim/after/plugin/luasnip.lua
@@ -3,9 +3,6 @@ if (not status) then return end
 
 require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
 
-ls.filetype_extend("typescript", { "javascript" })
-ls.filetype_extend("typescriptreact", { "javascript" })
-
 -- Keymap
 vim.keymap.set({ "i", "s" }, "<C-k>", function()
   if ls.expand_or_jumpable() then

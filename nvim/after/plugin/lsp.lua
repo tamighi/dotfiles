47aa2684f88a22dde1879fa96d50cd2507aa8a7a
tamighi <tamighi@hotmail.com>
2023-07-21 10:57:29 +0200

    No auto prettier

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -30,13 +30,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
 
     -- Format on save
-    if client.server_capabilities.documentFormattingProvider then
+    --[[ if client.server_capabilities.documentFormattingProvider then
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = vim.api.nvim_create_augroup("Format", { clear = true }),
         buffer = ev.buf,
         callback = function() vim.lsp.buf.format() end
       })
-    end
+    end ]]
   end,
 })
 

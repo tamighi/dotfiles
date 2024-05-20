dcf4092a82f609b061a9d26b76e0e3ac1eaf627d
tamighi <tomi-lys@live.fr>
2024-05-20 15:40:27 +0200

    new git push

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<leader>gg", vim.cmd.Git);
--- vim.keymap.set("n", "<C-p>", "<cmd>Git push<cr>");
 vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<cr>");
+vim.keymap.set("n", "<leader>gp", "<cmd>Git push<cr>");
 vim.keymap.set("n", "<leader>gl", "<cmd>Git log --oneline<cr>");
 vim.keymap.set("n", "<leader>gm", "<cmd>Git mergetool<cr>");

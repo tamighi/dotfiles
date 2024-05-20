00206f4277692d250337c09b51f29e14a52e1c71
tamighi <tomi-lys@live.fr>
2024-05-20 11:26:03 +0200

    clean

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -6,6 +6,9 @@ harpoon:setup()
 vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
 vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
+vim.keymap.set('n', '<C-n>', function() harpoon:list():next() end)
+vim.keymap.set('n', '<C-p>', function() harpoon:list():prev() end)
+
 for i = 1, 9 do
   vim.keymap.set('n', '<leader>' .. i, function() harpoon:list():select(i) end)
 end
--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -2,7 +2,6 @@ local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if (not status) then return end
 
 treesitter.setup {
-  -- A list of parser names, or "all"
   ensure_installed = {
     "markdown",
     "markdown_inline",
@@ -18,18 +17,9 @@ treesitter.setup {
     "php",
   },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
   highlight = {
-    enable = true,
-    disable = { "python" }
-  },
-  disable = { "python" },
-  indent = {
     enable = true
   }
 }

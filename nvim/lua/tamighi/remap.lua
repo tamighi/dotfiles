97f27b8614223d17176159b82d68d9745a906d07
tamighi <thomas.amighi@hotmail.com>
2024-11-10 08:24:28 +0100

    add keymap

--- nvim/lua/tamighi/remap.lua
+++ nvim/lua/tamighi/remap.lua
@@ -21,7 +21,10 @@ keymap.set("n", "<C-a>", "gg<S-v>G")
 keymap.set("n", "<C-w>v", "<C-w>v<C-w>l")
 
 -- Fast quit
-keymap.set("n", "<C-c>", ":q<Cr>")
+keymap.set("n", "<C-c>", ":x<Cr>")
+
+-- Fast save
+keymap.set("n", "<C-s>", ":w<Cr>")
 
 -- Clear search with <esc>
 keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
